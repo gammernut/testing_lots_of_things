a62a8e790436e9315990b3d4d2abb8f43e68de6f
Jacob Henson <gammernut@aol.com>
2020-06-29 14:19:33 -0500

    Initial commit

---
+++ program.py
@@ -0,0 +1,125 @@
+#
+# print('Hello world')
+# for i in range(5):
+#     print('holy shit' + str(i))
+#
+#
+# total = 0
+# for num in range(101):
+#     total = total + num
+# print(total)
+#
+#
+# print('holy shit i am')
+# for i in range(5):
+#     print('stoned' + str(i))
+#
+#
+# print('holy shit i am')
+# for i in range(12, 16):
+#         print('stoned' + str(i))
+#
+#
+# print('holy shit i am')
+# for i in range(0, 10, 2):
+#         print('stoned' + str(i))
+#
+#
+# print('holy shit i am')
+# for i in range(0, 10, -2):
+#         print('stoned' + str(i))
+#
+#
+# import sys
+# print('hello')
+# sys.exit()
+# print('goodbye')
+#
+#
+# #import pyperclip
+#
+# def hello ():
+#     print('howdy!')
+#     print('howdy!!!!')
+#     print('Hello there.')
+#
+# hello()
+# hello()
+# hello()
+#
+# def hello(name):
+#     print('Hello ' + name)
+#
+# hello('alice')
+# hello('bob')
+#
+#
+# def plusone (number):
+#     return number + 1
+#
+# new_number = plusone(5)
+# print(new_number)
+#
+#
+# print('Hello has ' + str(len('hello')) + ' letters int it.')
+#
+# print('Hello ', end='')
+# print('world')
+#
+# def div42by(divideby):
+#     try:
+#         return 42 / divideby
+#     except ZeroDivisionError:
+#         print('error: you tride too divide by zero.')
+#
+#
+# print(div42by(2))
+# print(div42by(12))
+# print(div42by(0))
+# print(div42by(1))
+#
+#
+#
+# print('how many cats do you have')
+# numcats = input ()
+# try:
+#     if int(numcats) >= 4:
+#         print('That is a lot of cats')
+#     else:
+#         print('That is not that many cats.')
+# except ValueError:
+#     print('you did not enter a number')
+#
+#
+# print('how many cats do you have')
+# numcats = input()
+# try:
+#     if int(numcats) >= 4:
+#         print('That is a lot of cats')
+#     elif int(numcats) <= 0:
+#         print('Please enter a Positive number')
+#     else:
+#         print('That is not that many cats.')
+# except ValueError:
+#     print('you did not enter a number')
+#
+# """
+#
+# while True:
+#     print('How many cats do you have?')
+#     numCats = input()
+#     try:
+#         if int(numCats) >= 4:
+#             print('That is a lot of cats.')
+#             break
+#         elif int(numCats) < 0:
+#             print('you cant have negative cats shit lord this isn\'t a death counter ')
+#
+#         else:
+#             print('That is not that many cats')
+#             break
+#     except ValueError:
+#         print('Thats not even a number asshole')
+#
+#
+#
