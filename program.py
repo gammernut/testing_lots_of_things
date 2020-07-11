33a2c39859d2930dacab1b345c2d01576ce97397
Jacob Henson <gammernut@aol.com>
2020-07-11 06:59:17 -0500

    sdfsdf

--- program.py
+++ program.py
@@ -1,9 +1,13 @@
 #
 # print('Hello world')
+#
 # for i in range(5):
-#     print('holy shit' + str(i))
+#     print('holy shit' + str(i))  # counts from 0
 #
 #
+# for i in range(5):
+#     print('holy shit' + str(i+1))  # counts from 1
+
 # total = 0
 # for num in range(101):
 #     total = total + num
@@ -121,3 +125,46 @@
 #     except ValueError:
 #         print('Thats not even a number asshole')
 #
+
+
+# class ass_hole:
+#
+#     def __init__(self, first_name, last_name):
+#         self.first_name = first_name
+#         self.last_name = last_name
+#
+#     def full_name(self):
+#         return f'{self.first_name} {self.last_name}'
+#
+#
+# me = ass_hole('Jacob', 'Henson')
+# gavin = ass_hole('Gavin', 'Springsteen')
+# brandon = ass_hole('Brandon', 'Rollins')
+# trey = ass_hole('Trey', 'Cooper')
+#
+#
+# print(me.full_name())
+# print(gavin.full_name())
+# print(brandon.full_name())
+# print(trey.full_name())
+
+
+# import sys
+# sysversion_info
+# print(f'hello from python{sys.version_info.major}.{sys.version_info.minor}.{sys.version_info.micro}!')
+
+
+# my_age = 24
+# user_age = int(input('how old are you'))
+#
+#
+# def difference(user_age):
+#     if user_age <= my_age:
+#         return my_age - user_age
+#     else:
+#         return (user_age - my_age) * 2
+#
+#
+# print(difference(user_age))
+# print(difference(user_age))
+
