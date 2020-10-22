f14d3220d5b74cc4a64c1e915950ac2296dbd490
Jacob Henson <Gammernut@aol.com>
2020-10-22 01:16:41 -0500

    lots
    Signed-off-by: Jacob Henson <Gammernut@aol.com>

--- program.py
+++ program.py
@@ -883,15 +883,18 @@
 # print_game_stats(games_won)
 # '''
 #
-# Split up the message on newline (\n) using the split builtin, then use the join builtin to stitch it together using a '|' (pipe).
+# Split up the message on newline
+# (\n) using the split builtin, then use the join builtin to stitch it together using a '|' (pipe).
 #
-# So Hello world:\nI am coding in Python :)\nHow awesome! would turn into: Hello world:|I am coding in Python :)|How awesome!
+# So Hello world:\nI am coding in Python :)\nHow awesome! would turn into: Hello world:|I am coding in Python :)|
+# How awesome!
 #
 # Your code should work for other message strings as well.
 #
 # Note that as opposed to previous Intro Bites your need to return the new string!
 #
-# Also note that, although we wanted you to learn about split and join here, there are more ways to pull this one off (check out the forum upon resolving this Bite ...)
+# Also note that, although we wanted you to learn about split and join here,
+# there are more ways to pull this one off (check out the forum upon resolving this Bite ...)
 # '''
 #
 #
@@ -1403,7 +1406,8 @@
 #
 #  print(slice_and_dice())
 #  """
-# Split up the message on newline (\n) using the split builtin, then use the join builtin to stitch it together using a '|' (pipe).
+# Split up the message on newline
+# (\n) using the split builtin, then use the join builtin to stitch it together using a '|' (pipe).
 # So Hello world:\
 #     nI
 # am
@@ -1690,7 +1694,9 @@
 #     print(a, 'much Numbers suck wow', end='')
 # Now it's your turn. Try this practice below.
 #
-# The core idea in this chapter was about dictionaries and data structures in general. Create a simple program that creates a dictionary called d such that the following runs without error and prints what is expected:
+# The core idea in this chapter was about dictionaries and data structures in general.
+# Create a simple program that creates a dictionary called d
+# such that the following runs without error and prints what is expected:
 #
 #  d = create d using core concepts above.
 #
